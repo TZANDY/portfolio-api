5783265746d625aab81dd7ace424420272ee2987
Andi_IS <tzandy07@gmail.com>
2026-08-30 22:29:17 -0500

    correccion cors

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,5 +1,5 @@
 import { Hono } from "hono";
-import { cors } from "hono/cors";
+// import { cors } from "hono/cors";
 import projectsRouter from '../routes/projects';
 import technologiesRouter from '../routes/technologies';
 import projectsTechnologiesRouter from '../routes/projectsTechnologies';
