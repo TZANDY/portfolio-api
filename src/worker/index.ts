ffee825117907f8a4c42988d39be5728e27742b1
Andi_IS <tzandy07@gmail.com>
2026-09-01 22:39:28 -0500

    agregando cors

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,5 +1,5 @@
 import { Hono } from "hono";
-// import { cors } from "hono/cors";
+import { cors } from "hono/cors";
 import projectsRouter from '../routes/projects';
 import technologiesRouter from '../routes/technologies';
 import projectsTechnologiesRouter from '../routes/projectsTechnologies';
@@ -7,16 +7,40 @@ import projectsTechnologiesRouter from '../routes/projectsTechnologies';
 type Bindings = {
   DATABASE_URL: string;
   DATABASE_AUTH_TOKEN: string;
+  ALLOWED_ORIGIN_PROD: string
+  ALLOWED_ORIGIN_PREVIEW: string
+  // API_SECRET_TOKEN: string
+  // ALLOWED_ORIGIN_DEV: string;
 };
 
 const app = new Hono<{ Bindings: Bindings }>();
 
 // const CLIENT_API_KEY = process.env.CLIENT_API_KEY;
 
-// app.use('*', cors({ 
-//     origin: 'https://www.andiis.dev',
-//     allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'] 
-// }));
+app.use('/*', cors({ 
+    origin: (origin,c)=>{
+      if (origin === c.env.ALLOWED_ORIGIN_PROD) {
+        return origin;
+      }
+
+      // if (origin === c.env.ALLOWED_ORIGIN_DEV) {
+      //   return origin;
+      // }
+
+      if (origin === c.env.ALLOWED_ORIGIN_PREVIEW) {
+        return origin;
+      }
+
+      // Permitir URLs de previsualización (Preview Deployments) de Vercel usando un Regex simple
+      if (origin.match(/^https:\/\/tu-app-.[a-zA-Z0-9-]+-vercel\.app$/)) {
+        return origin
+      }
+
+      return '';
+    },
+    allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowHeaders: ['Content-Type', 'Authorization'],
+}));
 
 app.get("/api/", (c) => c.json({ name: "Cloudflare" }));
 
